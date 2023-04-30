6980db1b53618f025a8f285e5ae405349151f4e3
Vaishak <vaishakjnair.98@gmail.com>
2023-04-30 12:27:01 +0530

    style changes

--- src/components/Description.js
+++ src/components/Description.js
@@ -7,8 +7,8 @@ const Description = ({ data, index, slideShow, playSlides }) => {
 
     return (
         <div className="description">
-            <Typography sx={{ mb: 3 }} variant="h3" component="h2">{data[index].title}</Typography>
-            <Typography variant="p" component="p" color={"text.secondary"} sx={{ mb: 3 }} >{data[index].desc}</Typography>
+            <Typography sx={{ mb: 3 }} variant="h3" component="h2" color="#707070">{data[index].title}</Typography>
+            <Typography variant="p" component="p" color="#B4B3B3" sx={{ mb: 3 }} >{data[index].desc}</Typography>
             <div className="play-container">
                 {slideShow ? <PauseCircleFilledIcon onClick={playSlides} className="play-btn" fontSize='x-large' /> : <PlayCircleFilledIcon onClick={playSlides} className="play-btn" fontSize='x-large' />}
             </div>
